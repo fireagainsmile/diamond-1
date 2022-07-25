31839e4c1d6ee20ff06b3d53b4bce779eecd682d
guanggg <952577382@qq.com>
2022-07-25 18:08:03 +0800

    add migration script

--- migrations/2_diamond.js
+++ migrations/2_diamond.js
@@ -1,10 +1,9 @@
 const storageApp = artifacts.require("storageFacet")
 const diamondApp = artifacts.require("Diamond")
 
-module.exports = function (deployer, accounts) {
-    const storageContract = deployer.deploy(storageApp);
-    console.log(storageContract.address);
-    const contractOwner = accounts[0];
-    // deployer.deploy(contractOwner, storageContract.address);
-    deployer.deploy(diamondApp, accounts[0], storageContract.address);
+module.exports = function (deployer, network,accounts) {
+    deployer.deploy(storageApp).then(function () {
+        const contractOwner = accounts[0];
+        return deployer.deploy(diamondApp, contractOwner, storageApp.address);
+    })
 }
